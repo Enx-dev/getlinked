0ea321bb0947d313c5393a59fd05ef8da1c346ff
Enx-Dev <enx.co21@gmail.com>
2023-09-23 22:13:05 +0100

    test

--- src/components/Home/Counter.tsx
+++ src/components/Home/Counter.tsx
@@ -1,19 +1,23 @@
 "use client";
 import { Unica_One } from "next/font/google";
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { twMerge } from "tailwind-merge";
-import { useEffect } from "react";
 type Props = {};
 const unicaOne = Unica_One({
   weight: "400",
   subsets: ["latin"],
 });
 
-const Counter = (props: Props) => {
-  const Ref = useRef(null);
-  const [sec, setSec] = useState<string | number>("00");
-  const [min, setMin] = useState<string | number>("00");
-  const [hr, sethr] = useState<string | number>("00");
+function useCountdown() {
+  const [{ hours, minutes, seconds }, setTimer] = useState<{
+    hours: string | number;
+    minutes: string | number;
+    seconds: string | number;
+  }>({
+    hours: "00",
+    minutes: "00",
+    seconds: "00",
+  });
 
   const getTimeRemaining = (e) => {
     const total = Date.parse(e) - Date.parse(String(new Date()));
@@ -27,41 +31,54 @@ const Counter = (props: Props) => {
       seconds,
     };
   };
+
   const startTimer = (e) => {
     let { total, hours, minutes, seconds } = getTimeRemaining(e);
     if (total >= 0) {
-      sethr(hours > 9 ? hours : "0" + hours);
-      setMin(minutes > 9 ? minutes : "0" + minutes);
-      setSec(seconds > 9 ? seconds : "0" + seconds);
+      setTimer({
+        hours: hours > 9 ? hours : "0" + hours,
+        minutes: minutes > 9 ? minutes : "0" + minutes,
+        seconds: seconds > 9 ? seconds : "0" + seconds,
+      });
     }
   };
-  const clearTimer = (e) => {
-    sethr("24");
-    setMin("00");
-    setSec("00");
 
-    if (Ref.current) clearInterval(Ref.current);
+  const clearTimer = (e) => {
+    // If you adjust it you should also need to
+    // adjust the Endtime formula we are about
+    // to code next
+    setTimer({
+      hours: "24",
+      minutes: "00",
+      seconds: "00",
+    });
+
+    // If you try to remove this line the
+    // updating of timer Variable will be
+    // after 1000ms or 1sec
     const id = setInterval(() => {
       startTimer(e);
     }, 1000);
-    Ref.current = id;
   };
+
   const getDeadTime = () => {
     let deadline = new Date();
     deadline.setHours(deadline.getHours() + 24);
     return deadline;
   };
+
   useEffect(() => {
     clearTimer(getDeadTime());
   }, []);
 
-  // Another way to call the clearTimer() to start
-  // the countdown is via action event from the
-  // button first we create function to be called
-  // by the button
   const onClickReset = () => {
     clearTimer(getDeadTime());
   };
+  return { hours, minutes, seconds };
+}
+
+const Counter = (props: Props) => {
+  const { hours, minutes, seconds } = useCountdown();
 
   return (
     <div
@@ -70,15 +87,15 @@ const Counter = (props: Props) => {
         "flex sm_tablet:pt-4 pt-8 gap-10"
       )}>
       <span className="flex items-baseline">
-        <p className={twMerge(unicaOne.className, "text-5xl")}>{hr}</p>
+        <p className={twMerge(unicaOne.className, "text-5xl")}>{hours}</p>
         <span className="text-xs">h</span>
       </span>
       <span className="flex items-baseline">
-        <p className="text-5xl">{min}</p>
+        <p className="text-5xl">{minutes}</p>
         <span className="text-xs">m</span>
       </span>
       <span className="flex items-baseline">
-        <p className="text-5xl">{sec}</p>
+        <p className="text-5xl">{seconds}</p>
         <span className="text-xs">s</span>
       </span>
     </div>
